ea4d27201ed1335284348cd118c852440c76b932
Abel Terefe <abel.terefe@trademarknow.com>
2019-07-22 14:49:31 +0300

    Add more private stuff

--- run-minikube.sh
+++ run-minikube.sh
@@ -6,9 +6,10 @@ fi
 kubectl config set-context $(kubectl config current-context) --namespace=jenkins
 
 if [[ -z $(kubectl get secrets | grep ssh-key-secret) ]]; then
-	kubectl create secret generic ssh-key-secret --from-file=ssh-privatekey=./id_rsa --from-file=ssh-publickey=./id_rsa.pub
-	kubectl create secret generic tmnow-ssh-secret --from-file=ssh-privatekey=/Users/abel/.ssh/id_rsa --from-file=ssh-publickey=/Users/abel/.ssh/id_rsa.pub
-    kubectl create secret generic sbt-tmnow-jfrog-credentials --from-file=tmnow.credentials=/Users/abel/.sbt/tmnow.credentials
+	kubectl create secret generic ssh-key-secret --from-file=ssh-privatekey=./private/ssh/id_rsa_1 --from-file=ssh-publickey=./private/ssh/id_rsa_1.pub
+	kubectl create secret generic tmnow-ssh-secret --from-file=ssh-privatekey=./private/ssh/id_rsa --from-file=ssh-publickey=./private/ssh/id_rsa.pub
+    kubectl create secret generic sbt-tmnow-jfrog-credentials --from-file=tmnow.credentials=./private/sbt/tmnow.credentials
+    kubectl create secret generic docker-tmnow-jfrog-credentials --from-file=docker.jfrog.credentials=./private/docker/config.json
 fi
 if [[ $(kubectl get configmaps --ignore-not-found) ]]; then
 	kubectl delete configmaps kube-master
